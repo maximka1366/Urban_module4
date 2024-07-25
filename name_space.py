1d4f7c5f988d40feafb04a219850d98b0ed802c9
maximka1366 <maximka66@mail.ru>
2024-07-25 10:09:00 +0300

    Add files via upload

---
+++ name_space.py
@@ -0,0 +1,9 @@
+
+# Вызов функции inner_function() приводит к ошибке .
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+test_function()
+inner_function()
